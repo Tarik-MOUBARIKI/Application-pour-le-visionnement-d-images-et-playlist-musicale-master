133ba86c747d556d3dbd19e41d6049ee88d9d67f
MTAREK19 <moubariki.tarik@gmail.com>
2021-04-16 11:18:24 +0000

    first commit

---
+++ Desktop/Cvbrief/Curriculum-Vitae/src/App.vue
@@ -0,0 +1,92 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Info personnelle</router-link> |
+    <router-link to="/formation">Formations</router-link> |
+    <router-link to="/experience">Experiences</router-link>|
+    <router-link to="/langues">Langues</router-link> |
+    <router-link to="/loisers">loisirs</router-link> |
+    <router-link to="/curriculum_Vitae">CV</router-link> 
+  </div>
+  <router-view/>
+</template>
+
+<style>
+body{
+  background-color: rgb(255, 255, 255);
+}
+form{
+  width: 50%;
+  margin-left: 25%;
+ 
+
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  text-decoration: none;
+}
+
+#nav a.router-link-exact-active {
+  color: #ff0909;
+}
+
+#CV{
+    width: 50%;
+    height: 90vh;
+    margin-left: 25%;
+  border: 1px solid white;
+}
+
+#infoPersonnel{
+    float: left;
+    text-align: left;
+    padding-left: 3%;
+    padding-top: 3%;
+        line-height: 0.7;
+}
+#title{
+    float: left;
+    padding-left: 25%;
+    width: 72%; 
+}
+#pics{
+
+    padding-left: 77%;
+}
+img{
+      width: 85%;
+}
+.formation{
+    width: 71%;
+    margin-top: 1%;
+    color: rgb(0, 0, 0);
+    float: left;
+    margin-left: 14%;
+    border: 1px solid;
+}
+table{
+  float: left;
+    margin-left: 4%;
+    margin-top: 3%;
+}
+td{
+  width: 20%;
+  color: rgb(0, 0, 0);
+}
+th{
+  width: 80%;
+
+}
+</style>
---
+++ Desktop/Cvbrief/Curriculum-Vitae/src/router/index.js
@@ -0,0 +1,53 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import PersonnelInfo from '../views/PersonnelInfo.vue'
+import Formation from '../views/Formation.vue'
+import Experience from '../views/Experience.vue'
+import Langues from '../views/Langues.vue'
+import Loisers from '../views/Loisers.vue'
+import Curriculum_Vitae from '../views/Curriculum_Vitae.vue'
+import Download from '../views/Download.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'PersonnelInfo',
+    component: PersonnelInfo
+  },
+  {
+    path: '/formation',
+    name: 'formation',
+    component: Formation
+  },
+  {
+    path: '/experience',
+    name: 'experience',
+    component: Experience
+  },
+  {
+    path: '/langues',
+    name: 'langues',
+    component: Langues
+  },
+  {
+    path: '/loisers',
+    name: 'loisers',
+    component: Loisers
+  },
+  {
+    path: '/curriculum_Vitae',
+    name: 'curriculum_Vitae',
+    component: Curriculum_Vitae
+  },
+  {
+    path: '/download',
+    name: 'Download',
+    component: Download
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
